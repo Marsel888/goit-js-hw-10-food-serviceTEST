9578d598cdd2c1e7ecc4899268af3a7aaab74f96
Vlad <82894174+Marsel888@users.noreply.github.com>
2021-10-13 12:17:19 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import './sass/main.scss';
-import template from './templates/template.hbs';
+
+import menu1 from './templates/menu1.hbs';
 
 import menu from './menu.json';
 
@@ -10,33 +10,52 @@ const Theme = {
 
 const toolBar = document.querySelector('#theme-switch-toggle');
 const body = document.querySelector('body');
-const menuIngridient = document.querySelector('.menu');
+const menuIngridient = document.querySelector('.menu')
+const liIngridients = document.querySelector('.tag-list__item')
 
 toolBar.addEventListener('change', tols);
 
 function tols(event) {
-  const checked = event.target.checked;
+ 
+  let checked = event.target.checked;
+  
+  // localStorage.setItem('check', checked);
 
-  if (checked) {
-    body.classList.replace(Theme.LIGHT, Theme.DARK);
+  if (checked == true) {
+  
+    body.classList.replace(Theme.LIGHT, Theme.DARK)
   } else {
-    body.classList.replace(Theme.DARK, Theme.LIGHT);
+   
+    body.classList.replace(Theme.DARK, Theme.LIGHT)
+
   }
-  const currentTheme = body.classList.value;
-  localStorage.setItem('theme', currentTheme);
+  const darkTheme = body.classList.value;
+  localStorage.setItem('theme', darkTheme);
 }
 function save() {
   const theme = localStorage.getItem('theme');
-  if (theme === 'dark-theme') {
-    body.classList.add(Theme.LIGHT);
-    toolBar.checked = false;
-  } else {
+  if (theme === Theme.DARK) {
     body.classList.add(Theme.DARK);
     toolBar.checked = true;
+  } else {
+    body.classList.add(Theme.LIGHT);
+    toolBar.checked = false;
   }
 }
 save();
 
-const repository = template(menu);
 
-menuIngridient.insertAdjacentHTML('beforeend', repository);
+const repository = menu1(menu)
+
+menuIngridient.insertAdjacentHTML('beforeend', repository)
+// function name(menu) {
+
+//   const test1 = menu.map(menu1).join('')
+//   console.log(test1);
+//      return menuIngridient.insertAdjacentHTML('afterbegin', test1)
+
+
+   
+// }
+
+// console.log(name(menu));
